d68acba900be8ff051b8995159dc4c10c012b0bf
mtaner <mervetaner88@gmail.com>
2016-05-19 11:40:25 +0100

    Sixth commit

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'sinatra', "~> 1.4.7"
+gem 'execjs', "~> 2.6.0"
--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'execjs'
 
 get '/' do
   erb :home_page
@@ -6,4 +7,8 @@ end
 
 get '/rps' do
   erb :rps
+end
+
+post '/rps' do
+  ExecJS.eval "console.log('Computer chose:' + computerChoice + '.' + result)"
 end
