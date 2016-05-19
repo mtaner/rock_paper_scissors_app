f0918d273277314c0b1386b7f99350b77512a030
mtaner <mervetaner88@gmail.com>
2016-05-19 14:05:55 +0100

    11th commit updated

--- main.rb
+++ main.rb
@@ -7,5 +7,5 @@ end
 post "/rps" do
     @human_choice = params[:human_choice]
     erb :rps
-    "You submitted #{params[:human_choice].inspect}. Computer chose #{@computer_choice}.#{@result}"
+    "You submitted #{params[:human_choice].inspect}. Computer chose #{@computer_choice}. #{@result}"
 end
